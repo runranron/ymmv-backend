95ef30c1225d790241479c22a02c1fa026a86979
runranron <williamrpelton@gmail.com>
2018-06-29 13:18:35 -0500

    heroku

--- bookmarks/bookmarks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Bookmark, PersonalBookmark
-
-# Register your models here.
-admin.site.register(Bookmark)
-admin.site.register(PersonalBookmark)
